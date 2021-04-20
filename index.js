c908b2375f3d862a78f9a1f02b69ef373a0a072b
Dan Levy <danlevy.ca@icloud.com>
2021-04-20 00:50:11 -0400

    Correction de la validation.

--- index.js
+++ index.js
@@ -7,31 +7,82 @@
 /* eslint-disable strict */
 /* eslint-disable no-console */
 
-document.forms.infos.addEventListener('submit', Validation);
+document.formulaire.addEventListener('submit', Validation);
+
+const utilisateur = {};
 
 /**
  * Permet la validation du formulaire.
  */
-function Validation() {
-  /** @type {Array} Enregistre les messages d'erreurs. */
-  const errors = [];
-  /** @type {Object} Saisie du prénom. */
-  const fname = document.getElementById('fname');
-  /** @type {Object} Saisie du nom de famille. */
-  const lname = document.getElementById('lname');
-  /** @type {Object} Saisie de la date de naissance. */
-  const birth = document.getElementById('birth');
-  /** @type {Object} Saisie du status. */
-  const status = document.getElementById('status');
+function Validation(e) {
+  const form = document.getElementById('form');
+  const estDisplay = !form.className.includes('d-none');
 
-  // Ajoute au vecteur d'erreurs les messages de chaque champs.
-  errors.push(CheckText(fname));
-  errors.push(CheckText(lname));
-  errors.push(CheckDate(birth));
-  // errors.push(CheckEmpty(birth));
+  // Vrai si le formulaire est à valider
+  if (estDisplay) {
+    /** @type {Array} Enregistre les messages d'erreurs, les labels et les saisis. */
+    const errors = [];
+    const inputs = document.formulaire.getElementsByClassName('form-control');
 
-  // Nettoie le vecteur de toute valeure null ou undefined.
-  errors.filter((x) => x != null);
+    // Ajouter les messages d'erreurs au vecteur après validation.
+    for (let i = 0; i < inputs.length; i += 1) {
+      const input = inputs[i];
+      if (input.name === 'naissance') {
+        const validation = CheckDate(input);
+        if (validation) {
+          errors.push(validation);
+        }
+      } else {
+        const validation = CheckText(input);
+        if (validation) {
+          errors.push(validation);
+        }
+      }
+    }
+
+    // Retire les anciens messages d'erreurs.
+    const lastList = document.getElementById('erreurs');
+    const lastLabels = document.getElementsByClassName('erreur');
+    if (lastList) {
+      lastList.remove();
+    }
+    if (lastLabels.length) {
+      while (lastLabels.length) {
+        lastLabels[0].remove();
+      }
+    }
+
+    // Vrai, si le vecteur contient des messages d'erreurs.
+    if (errors.length) {
+      const liste = document.createElement('ol');
+      liste.id = 'erreurs';
+      for (let i = 0; i < errors.length; i += 1) {
+        const [error, label] = errors[i];
+        // On ajoute le message d'erreurs à la liste récapitulative
+        const li = document.createElement('li');
+        const message = document.createTextNode(error);
+        li.appendChild(message);
+        liste.appendChild(li);
+
+        // On ajout le message à chaque saisie après le label
+        const errorLabel = document.createElement('label');
+        errorLabel.setAttribute('for', `${label.attributes.for}`);
+        errorLabel.classList.add('erreur');
+        errorLabel.textContent = error;
+        label.parentElement.insertBefore(errorLabel, label.nextSibling);
+      }
+      document.getElementById('liste-erreurs').append(liste);
+    } else {
+      inputs.forEach((input) => {
+        const { name } = input;
+        utilisateur.name = input.value;
+      });
+    }
+  } else {
+    // Faux si le quiz est à valider
+  }
+
+  e.preventDefault();
 }
 
 /**
@@ -40,15 +91,84 @@ function Validation() {
  * @param {Object} champs Champs de saisie sur lequel on veut appliquer la validation.
  * @return {String} Le message d'erreur correspondant ou dans le cas échant undefined.
  */
-function CheckText(champs) {
-  const label = document.querySelector(`label[for=${champs.name}]`).textContent;
-  if (champs.value === '') {
-    return `Le champs ${label} ne peux être vide.`;
+function CheckText(input) {
+  if (input.value === '') {
+    const label = GetLabel(input);
+    return [`Le champs ${label.textContent} ne peux être vide.`, label];
   }
-  if (!/[^a-zA-Z]/.test(champs.value)) {
-    return `Le champs ${label} ne peux contenir des caractères autres que des lettres.`;
+  if (/[^a-zA-Z]/.test(input.value)) {
+    const label = GetLabel(input);
+    return [`Le champs ${label.textContent} ne peux contenir des caractères autres que des lettres.`, label];
   }
+}
+
+/**
+ * Validation de la date.
+ * La date doit être inférieur à aujourd'hui, l'année, le mois
+ * et le jour doivent être valide et le mois de février doit correspondre
+ * aux années bissextiles.
+ *
+ * @param {Object} input Champs de la date.
+ * @return {String} Message d'erreur correspondant.
+ */
+function CheckDate(input) {
+  const rawDate = input.value;
+  if (rawDate === '') {
+    const label = GetLabel(input);
+    return [`Le champs ${label.textContent} ne peut être vide.`, label];
+  }
+  if (!/^(\d\d)[/](\d\d)[/](\d{4})$/.test(rawDate)) {
+    const label = GetLabel(input);
+    return [`Le format de ${label.textContent} est incorrect.`, label];
+  }
+  const date = input.value.split('/');
+  for (let i = 0; i < date.length; i += 1) date[i] = +date[i];
+  const [jour, mois, annee] = date;
+  const ajd = new Date();
+  const ajdAnnee = ajd.getFullYear();
+  const ajdMois = ajd.getMonth() + 1;
+  const ajdJour = ajd.getDate();
+  if (annee < 1900) {
+    const label = GetLabel(input);
+    return ['L\'année de naissance est trop lointaine.', label];
+  }
+  if (annee > ajdAnnee) {
+    const label = GetLabel(input);
+    return ['L\'année de naissance ne peut être dans le futur.', label];
+  }
+  if (annee === ajdAnnee && mois > ajdMois) {
+    const label = GetLabel(input);
+    return ['Le mois de naissance ne peut être dans le futur.', label];
+  }
+  if (annee === ajdAnnee && mois === ajdMois && jour > ajdJour) {
+    const label = GetLabel(input);
+    return ['Le jour de naissance ne peut être dans le futur.', label];
+  }
+  if (mois < 1 || mois > 12) {
+    const label = GetLabel(input);
+    return ['Le mois de naissance doit être comprit entre 1 et 12.', label];
+  }
+  if (jour < 1 || jour > 31) {
+    const label = GetLabel(input);
+    return ['Le jour de naissance doit être comprit entre 1 et 31.', label];
+  }
+  if (jour === 31 && [4, 6, 9, 11].includes(mois)) {
+    const label = GetLabel(input);
+    return ['Le mois de naissance ne contient pas 31 jours.', label];
+  }
+  if (mois === 2) { // Vérification d'une année bissextile
+    const estBissextile = annee % 4 === 0 && (annee % 100 !== 0 || annee % 400 === 0);
+    if (jour > 29 || (jour === 29 && !estBissextile)) {
+      const label = GetLabel(input);
+      return ['Le jour de naissance est invalide pour le mois de février.', label];
+    }
+  }
+
   return undefined;
+}
+
+function GetLabel(input) {
+  return document.querySelector(`label[for=${input.id}]`);
 }
 
 /**
@@ -80,56 +200,3 @@ function DateNaissance(input, e) {
     champs.value += '/';
   }
 }
-
-/**
- * Validation de la date.
- * La date doit être inférieur à aujourd'hui, l'année, le mois
- * et le jour doivent être valide et le mois de février doit correspondre
- * aux années bissextiles.
- *
- * @param {Object} champs Champs de la date.
- * @return {String} Message d'erreur correspondant.
- */
-function CheckDate(champs) {
-  const rawDate = champs.value;
-  if (!/^(\d\d)[/](\d\d)[/](\d{4})$/.test(rawDate)) {
-    return 'Le format de la date est incorrect.';
-  }
-  const date = champs.value.split('/');
-  for (let i = 0; i < date.length; i += 1) date[i] = +date[i];
-  const [jour, mois, annee] = date;
-  const ajd = new Date();
-  const ajdAnnee = ajd.getFullYear();
-  const ajdMois = ajd.getMonth() + 1;
-  const ajdJour = ajd.getDate();
-  if (annee > ajdAnnee) {
-    return 'L\'année de naissance ne peut être dans le futur.';
-  }
-  if (annee === ajdAnnee && mois > ajdMois) {
-    return 'Le mois de naissance ne peut être dans le futur.';
-  }
-  if (annee === ajdAnnee && mois === ajdMois && jour > ajdJour) {
-    return 'Le jour de naissance ne peut être dans le futur.';
-  }
-  if (mois < 1 || mois > 12) {
-    return 'Le mois de naissance doit être comprit entre 1 et 12.';
-  }
-  if (jour < 1 || jour > 31) {
-    return 'Le jour de naissance doit être comprit entre 1 et 31.';
-  }
-  if (jour === 31 && [4, 6, 9, 11].includes(mois)) {
-    return 'Le mois de naissance ne contient pas 31 jours.';
-  }
-  if (mois === 2) { // Vérification d'une année bissextile
-    const estBissextile = annee % 4 === 0 && (annee % 100 !== 0 || annee % 400 === 0);
-    if (jour > 29 || (jour === 29 && !estBissextile)) {
-      return 'Le jour de naissance est invalide pour le mois de février.';
-    }
-  }
-
-  return undefined;
-}
-
-// function CheckEmpty(champs) {
-
-// }
